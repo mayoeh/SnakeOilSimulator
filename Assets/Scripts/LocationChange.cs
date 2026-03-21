773c894bcf7ae8359f607ae9d766f19619ab91fb
mayoeh <mayoeh43@gmail.com>
2026-03-21 18:37:54 -0400

    Start Scene
    Added start scene, connected to bootstrap and shop scenes

---
+++ Assets/Scripts/LocationChange.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LocationChange : MonoBehaviour
+{
+    public string sceneName;
+    public GameState currentState;
+    public void changeScene()
+    {
+        SceneManager.LoadScene(sceneName);
+        Debug.Log("Tried to change scenes");
+    }
+
+    public void GoToCustomer()
+    {
+        currentState = GameState.Needs;
+        SceneManager.LoadScene("Customer");
+    }
+}
