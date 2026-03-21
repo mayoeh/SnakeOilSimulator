3c49bcd54d4de0c31a34e64da78afb94490ad856
mayoeh <mayoeh43@gmail.com>
2026-03-21 12:59:06 -0400

    Bootstrap Scene
    Added bootstrap scene and script to create and allow persistent objects later

---
+++ Assets/Scripts/Bootstrap.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Bootstrap : MonoBehaviour
+{
+    public string nextSceneName = "Start";
+
+    private void Awake()
+    {
+        InitializeManagers();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(LoadNextScene());
+    }
+
+    private IEnumerator LoadNextScene()
+    {
+        yield return null; // wait one frame
+        SceneManager.LoadScene(nextSceneName);
+    }
+
+    private void InitializeManagers()
+    {
+        // ADD LATER
+        // if (GameManager.Instance == null)
+        //     new GameObject("GameManager").AddComponent<GameManager>();
+
+        // if (InventoryManager.Instance == null)
+        //     new GameObject("InventoryManager").AddComponent<InventoryManager>();
+
+        // if (CustomerManager.Instance == null)
+        //     new GameObject("CustomerManager").AddComponent<CustomerManager>();
+    }
+}
